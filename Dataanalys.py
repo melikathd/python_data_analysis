824566510d1fa015d7c91adb0028f74f3eb05e9a
melikathd <melika.thd@icloud.com>
2026-01-20 18:47:37 +0100

    Update Dataanalys.py

--- Dataanalys.py
+++ Dataanalys.py
@@ -25,23 +25,6 @@ print("the longest name is :", result)
 
 
 
-def analyz_data(names, numbers):
-    filtered_numbers = []
-    long_names = []
-    longest_name = ""
-    for name in names:
-        name = name.strip()
-        if len(name) > 5:
-            long_names.append(name)
-            if len(name) > len(longest_name):
-                longest_name = name
-    for number in numbers:
-        if number % 2 != 0 and number > 12:
-            filtered_numbers.append(number)
-    print("odd numbers greater than 12:", filtered_numbers)
-    print("names longer than 5 letters:", long_names)
-    return longest_name
-
 
 names = ["Maryam", "lila", "mohammadreza", "lilin"]
 numbers = [13, 15, 4, 5, 25, 21]
